9ff7fbb52f8e33cba03b19a63aba1640ee359d35
Liz Vanderkloot <liz@lizvdk.com>
2016-05-06 14:57:43 -0400

    Add SvgToBezierConverter
    SVG Converter by Nate Cook: http://nshipster.com/nsscanner/
    Used with permission

---
+++ AnimationClubSwirl/SvgToBezierConverter.swift
@@ -0,0 +1,62 @@
+//
+//  SvgToBezierConverter.swift
+//  AnimationClubSwirl
+//
+//  SVG Converter by Nate Cook: http://nshipster.com/nsscanner/
+//  Used with permission
+//
+
+import UIKit
+
+public class SvgToBezierConverter {
+
+    func bezierPathFromSVGPath(str: String) -> UIBezierPath {
+        let scanner = NSScanner(string: str)
+
+        // skip commas and whitespace
+        let skipChars = NSMutableCharacterSet(charactersInString: ",")
+        skipChars.formUnionWithCharacterSet(NSCharacterSet.whitespaceAndNewlineCharacterSet())
+        scanner.charactersToBeSkipped = skipChars
+
+        // the resulting bezier path
+        let path = UIBezierPath()
+
+        // instructions code can be upper- or lower-case
+        let instructionSet = NSCharacterSet(charactersInString: "MCSQTAmcsqta")
+        var instruction: NSString?
+
+        // scan for an instruction code
+        while scanner.scanCharactersFromSet(instructionSet, intoString: &instruction) {
+            var x = 0.0, y = 0.0
+            var points: [CGPoint] = []
+
+            // scan for pairs of Double, adding them as CGPoints to the points array
+            while scanner.scanDouble(&x) && scanner.scanDouble(&y) {
+                points.append(CGPoint(x: x, y: y))
+            }
+
+            // new point for bezier path
+            switch instruction ?? "" {
+            case "M":
+                path.moveToPoint(points[0])
+            case "C":
+                path.addCurveToPoint(points[2], controlPoint1: points[0], controlPoint2: points[1])
+            case "c":
+                path.addCurveToPoint(path.currentPoint.offset(points[2]),
+                                     controlPoint1: path.currentPoint.offset(points[0]),
+                                     controlPoint2: path.currentPoint.offset(points[1]))
+            default:
+                break
+            }
+        }
+
+        return path
+    }
+
+}
+
+extension CGPoint {
+    func offset(p: CGPoint) -> CGPoint {
+        return CGPoint(x: x + p.x, y: y + p.y)
+    }
+}
